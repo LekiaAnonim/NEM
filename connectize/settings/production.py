9aa1e264bf03f2d647174fe8730cb376433dd211
LekiaAnonim <lekiaprosper@gmail.com>
2024-02-16 19:31:20 +0100

    added email, and removed backgroud image from learn more

--- connectize/settings/production.py
+++ connectize/settings/production.py
@@ -4,7 +4,19 @@ import environ
 
 DEBUG = False
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 ALLOWED_HOSTS = ["nem-production.up.railway.app", "connectize.co"]
+
+EMAIL_HOST = 'smtp.elasticemail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 2525
+EMAIL_HOST_USER = env('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
 try:
     from .local import *
