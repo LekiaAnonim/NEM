279df3f4d62f426c826d4f6cd00416d849272b4d
LekiaAnonim <lekiaprosper@gmail.com>
2024-03-09 22:49:03 +0100

    added wowjs for scroll animation

---
+++ static/js/scrollreveal.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.home-hero');
+ScrollReveal().reveal('.product-mockup');
+ScrollReveal().reveal('.about-summary');
+ScrollReveal().reveal('.features-services');
+ScrollReveal().reveal('.site-metrics');
+ScrollReveal().reveal('.about-summary');
+ScrollReveal().reveal('.testimonial-container');
