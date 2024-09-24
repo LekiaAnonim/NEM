2092dc6a11a37bf383358a1561e06a77ac1987e6
LekiaAnonim <lekiaprosper@gmail.com>
2024-09-24 01:53:02 -0400

    add brevo smtp settings

--- connectize/settings/production.py
+++ connectize/settings/production.py
@@ -2,16 +2,17 @@ from .base import *
 
 import environ
 
-DEBUG = True
+
 
 env = environ.Env(
     DEBUG=(bool, True)
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = ["nem-production.up.railway.app", "connectize.co", 'localhost:3000', 'connectize-drab.vercel.app']
 
-EMAIL_HOST = 'smtp.elasticemail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 2525
 EMAIL_HOST_USER = env('DEFAULT_FROM_EMAIL')
